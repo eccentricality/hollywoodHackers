10f1504874684a694626906bcedb379eab8c74a4
eccentricality <youngmin13@gmail.com>
2021-06-07 16:38:38 -0400

    rerouted search function to target single search container

--- assets/script.js
+++ assets/script.js
@@ -60,6 +60,20 @@ let movieContainer = [
         tomatoRating: document.getElementById('tomatoRating4'),
         metaRating: document.getElementById('metaRating4'),
         boxOffice: document.getElementById('boxOffice4'),
+    },
+    {
+        movieSlot: '5',
+        moviePoster: document.getElementById('loadingScreenImg5'),
+        movieRated: document.getElementById('movieRated5'),
+        firstActor: document.getElementById('firstActor5'),
+        secondActor: document.getElementById('secondActor5'),
+        thirdActor: document.getElementById('thirdActor5'),
+        fourthActor: document.getElementById('fourthActor5'),
+        revSummary: document.getElementById('revSummary5'),
+        imdbRating: document.getElementById('imdbRating5'),
+        tomatoRating: document.getElementById('tomatoRating5'),
+        metaRating: document.getElementById('metaRating5'),
+        boxOffice: document.getElementById('boxOffice5'),
     }
 ]
 
@@ -68,7 +82,7 @@ function getApi(movie) {
     let requestUrl = 'https://www.omdbapi.com/?apikey=2b59165d&t=' + movie;
     let searchedMovie = movie;
 
-    getMovReview1(searchedMovie);
+    getMovReview5(searchedMovie);
 
     fetch(requestUrl)
         .then(function (response) {
@@ -77,111 +91,21 @@ function getApi(movie) {
         .then(function (data) {
             let movActors = data.Actors.split(',');
 
-                movieContainer[0].moviePoster.src = data.Poster;
-                movieContainer[0].movieRated.innerText = 'Rated ' + data.Rated;
-                movieContainer[0].firstActor.innerText = movActors[0];
-                movieContainer[0].secondActor.innerText = movActors[1];
-                movieContainer[0].thirdActor.innerText = movActors[2];
-                movieContainer[0].fourthActor.innerText = movActors[3];
-                movieContainer[0].imdbRating.innerText = 'IMDB: ' + data.Ratings[0].Value;
-                movieContainer[0].tomatoRating.innerText = 'Rotten Tomato: ' + data.Ratings[1].Value;
-                movieContainer[0].metaRating.innerText = 'Meta Critic: ' + data.Ratings[2].Value;
-                movieContainer[0].boxOffice.innerText = 'Box Office: ' + data.BoxOffice;
+                movieContainer[4].moviePoster.src = data.Poster;
+                movieContainer[4].movieRated.innerText = 'Rated ' + data.Rated;
+                movieContainer[4].firstActor.innerText = movActors[0];
+                movieContainer[4].secondActor.innerText = movActors[1];
+                movieContainer[4].thirdActor.innerText = movActors[2];
+                movieContainer[4].fourthActor.innerText = movActors[3];
+                movieContainer[4].imdbRating.innerText = 'IMDB: ' + data.Ratings[0].Value;
+                movieContainer[4].tomatoRating.innerText = 'Rotten Tomato: ' + data.Ratings[1].Value;
+                movieContainer[4].metaRating.innerText = 'Meta Critic: ' + data.Ratings[2].Value;
+                movieContainer[4].boxOffice.innerText = 'Box Office: ' + data.BoxOffice;
         });
 }
 
 // generates random movie from container of 20 to append on home page
 function genRandMov() {
-    let randMovContainer = [
-        'Tenet',
-        'Avengers Endgame',
-        'Marriage Story',
-        'Honeyland',
-        'Portrait of a Lady on Fire',
-        'Waves',
-        'Uncut Gems',
-        'Midsommar',
-        'Avengers: Infinity War',
-        'Little Women',
-        'The Farewell',
-        'Knives Out',
-        'John Wick',
-        'Hereditary',
-        'Us',
-        'A Hidden Life',
-        'Ad Astra',
-        'Atlantics',
-        'High Life',
-        '1917',
-        'Jojo Rabbit',
-        'BlacKkKlansman',
-        'Spider-Man: Into the Spider-Verse',
-        'Bad Times at the El Royale',
-        'Vice',
-        'Black Panther',
-        'Sorry to Bother You',
-        'Instant Family',
-        'Ant-Man and the Wasp',
-        'The Matrix',
-        'Green Book',
-        'Ready Player One',
-        'Game Night',
-        'Star Wars',
-        'The Empire Strikes Back',
-        'Return of the Jedi',
-        'Force Awakens',
-        'The Last Jedi',
-        'Rise of Skywalker',
-        'Pulp Fiction',
-        'Reservoir Dogs',
-        'Inglourious Basterds',
-        'Django Unchained',
-        'Die Hard',
-        'Tenet',
-        'Avengers Endgame',
-        'Marriage Story',
-        'Honeyland',
-        'Portrait of a Lady on Fire',
-        'Waves',
-        'Uncut Gems',
-        'Midsommar',
-        'Avengers: Infinity War',
-        'Little Women',
-        'The Farewell',
-        'Knives Out',
-        'John Wick',
-        'Hereditary',
-        'Us',
-        'A Hidden Life',
-        'Ad Astra',
-        'Atlantics',
-        'High Life',
-        '1917',
-        'Jojo Rabbit',
-        'BlacKkKlansman',
-        'Spider-Man: Into the Spider-Verse',
-        'Bad Times at the El Royale',
-        'Vice',
-        'Black Panther',
-        'Sorry to Bother You',
-        'Instant Family',
-        'Ant-Man and the Wasp',
-        'The Matrix',
-        'Green Book',
-        'Ready Player One',
-        'Game Night',
-        'Star Wars',
-        'The Empire Strikes Back',
-        'Return of the Jedi',
-        'Force Awakens',
-        'The Last Jedi',
-        'Rise of Skywalker',
-        'Pulp Fiction',
-        'Reservoir Dogs',
-        'Inglourious Basterds',
-        'Django Unchained',
-        'Die Hard',
-    ]
     let randGenMov = randMovContainer[Math.floor(Math.random() * randMovContainer.length)];
 
     return randGenMov; 
@@ -242,6 +166,17 @@ function getMovReview4(movie) {
 // append portion of the get movie review
 function appendMovReview4(data) {
     movieContainer[3].revSummary.innerText = data;
+}
+
+function getMovReview5(movie) {
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=' + movie + '&api-key=fx2dHCz2zs4KD2BRnWCY4iAkqczVc9v9')
+    .then((response) => response.json())
+    .then((data) => appendMovReview5(data.results[0].summary_short))
+}
+
+// append portion of the get movie review
+function appendMovReview5(data) {
+    movieContainer[4].revSummary.innerText = data;
 }
 
 function getHomeApi1(movie) {
